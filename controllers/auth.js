eff1caf86fb628b80656ff55a9d256fa9b443d68
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-12-01 21:56:19 -0500

    Implementación de google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const { response } = require("express");
 const Usuairo = require("../models/usuario");
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
@@ -17,25 +18,25 @@ const login = async (req, res = response) => {
     }
 
     //Verificar si el usuario está activo
-    if(!usuario.estado){
-        return res.status(400).json({
-            msg: 'El usuario no existe ;) active'
-        });
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "El usuario no existe ;) active",
+      });
     }
 
     //Verificar la contraseña
     const validPassword = bcryptjs.compareSync(password, usuario.password);
 
-    if(!validPassword){
-        return res.status(400).json({
-            msg: 'Usuario/Password ;) no son correctos'
-        });
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Usuario/Password ;) no son correctos",
+      });
     }
-    
+
     const token = await generarJWT(usuario.id);
     res.status(200).json({
       usuario,
-      token
+      token,
     });
   } catch (error) {
     console.log("Error en auth", error);
@@ -45,6 +46,52 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { nombre, correo, img } = await googleVerify(id_token);
+
+    let usuario = await Usuairo.findOne({ correo });
+
+    if (!usuario) {
+      //tengo que crear
+      const data = {
+        nombre,
+        correo,
+        img,
+        password: ":?",
+        google: true,
+        estado: true,
+        rol: 'USER_ROL'
+      };
+      usuario = new Usuairo(data);
+      await usuario.save();
+    }
+
+    //si el usuario
+    if (usuario.estado == false) {
+      res.status(401).json({
+        msg: "Hable con el administrador usuario bloqueado",
+      });
+    }
+    const token = await generarJWT(usuario.id);
+    res.status(200).json({
+      usuario,
+      token
+    });
+
+
+  } catch (error) {
+    console.log("Ocurrió un error", error);
+    res.status(400).json({
+      ok: false,
+      msg: "El token no se pudo verificar",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,14 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
-const {login} = require('../controllers/auth');const { validarCampos } = require('../middlewares/validar-campos');
+const {login, googleSignIn} = require('../controllers/auth');const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
 router.post('/login', [
     check('correo', 'El correo es necesario').isEmail(),
     check('password', 'La contraseña tiene que se mayor a 5 caracteres').isLength({min: 5}),
     validarCampos
 ],login);
+router.post('/google', [
+    check('id_token', 'El Id token es necesario').notEmpty(),
+    validarCampos
+],googleSignIn);
 module.exports = router;
